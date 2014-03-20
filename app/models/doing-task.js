c83c02639934869e04fc53eae1049debfd1966e9
Jeremy Greer <jeremygreer@reergymerej.com>
2014-03-20 07:53:21 -0400

    add unit tests for DoingTask

---
+++ app/models/doing-task.js
@@ -0,0 +1,63 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+
+// REF: http://mongoosejs.com/docs/guide.html
+var DoingTaskSchema = new Schema({
+    user: {
+        type: Schema.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    start: {
+        type: Date,
+        default: Date.now
+    },
+    end: {
+        type: Date,
+        default: null
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String
+    }
+});
+
+/**
+ * Validations
+ */
+DoingTaskSchema.path('user').validate(function (user) {
+    return user && user.length;
+}, 'TodoTasks must have a user.');
+
+
+DoingTaskSchema.statics.load = function (id, cb) {
+
+    // Get the first MongoDB document that matches
+    // this criteria.
+    // REF: http://docs.mongodb.org/manual/reference/method/db.collection.findOne/
+    this.findOne({
+        _id: id
+    })
+    // This add a couple fields to the document (record).
+    // REF: http://mongoosejs.com/docs/populate.html
+    .populate('user')
+
+    // exec runs after populate has finished.
+    // Remember, node is all about asynchronous code.
+    // http://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
+    .exec(cb);
+};
+
+// Now that the schema is set up, register it with Mongoose
+// so it can be used when needed.
+// REF: http://mongoosejs.com/docs/models.html
+mongoose.model('DoingTask', DoingTaskSchema);
---
+++ test/mocha/doing/model.js
@@ -0,0 +1,118 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var should = require('should'),
+    mongoose = require('mongoose'),
+    User = mongoose.model('User'),
+    DoingTask = mongoose.model('DoingTask');
+
+//Globals
+var user;
+var doingTask;
+
+//The tests
+describe('<Unit Test>', function() {
+    describe('Model DoingTask:', function() {
+
+        // Before each test, create a new user and
+        // save it.  We need this because the article
+        // requires an associated user.  This happens
+        // before each "it" function.
+        beforeEach(function (done) {
+
+            user = new User({
+                name: 'Full name',
+                email: 'test@test.com',
+                username: 'user',
+                password: 'password'
+            });
+
+            user.save(function () {
+                doingTask = new DoingTask({
+                    user: user,
+                    description: 'some description',
+                    category: 'some category'
+                });
+
+                done();
+            });
+        });
+
+        describe('Method Save', function() {
+            it('should be able to save without problems', function (done) {
+
+                return doingTask.save(function (err) {
+                    // REF: https://github.com/visionmedia/should.js/#chaining-assertions
+                    should.not.exist(err);
+                    done();
+                });
+            });
+
+            it('should throw an error when saving without a user', function (done) {
+                doingTask.user = undefined;
+
+                return doingTask.save(function (err) {
+                    should.exist(err);
+                    done();
+                });
+            });
+
+            it('should throw an error when saving without a description', function (done) {
+                doingTask.description = undefined;
+
+                return doingTask.save(function (err) {
+                    should.exist(err);
+                    done();
+                });
+            });
+        });
+
+        describe('fields', function () {
+            
+            it('should have a start date', function (done) {
+                doingTask.should.have.property('start');
+                done();
+            });
+
+            it('should have a description', function (done) {
+                doingTask.should.have.property('description');
+                done();
+            });
+        });
+
+        describe('deleting', function () {
+
+            it('should remove without any problems', function (done) {
+                var _id = doingTask._id;
+
+                doingTask.remove(function () {
+                    DoingTask.find({ _id: _id }, function (err, doingTasks) {
+                        doingTasks.should.have.length(0);
+                        done();
+                    });
+                });
+            });
+        });
+
+        // This runs after each test.
+        afterEach(function(done) {
+            doingTask.remove(function () {
+                user.remove(function () {
+                    done();
+                });
+            });
+        });
+        after(function(done) {
+            // QUESTION: What's with the exec?
+            // It looks like you can do remove(fn) or remove().exec
+            // to differentiate between async/sync processes.
+            // We'll learn more about this when we brush up
+            // on MongoDB.
+            // REF: http://stackoverflow.com/a/10266789/1319850
+            // User.remove().exec();
+            done();
+        });
+    });
+});
