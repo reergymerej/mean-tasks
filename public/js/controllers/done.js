013c0c5917ee4862fbcd636268319c285846066f
Jeremy Greer <jeremygreer@reergymerej.com>
2014-04-09 22:58:47 -0400

    fix missing category in chart

--- public/js/controllers/done.js
+++ public/js/controllers/done.js
@@ -50,7 +50,8 @@ angular.module('done.tasks').controller('DoneCtrl', ['$scope', '$filter', '$stat
 
         var success = function (tasks) {
             $scope.tasks = tasks;
-            makePie(tasks);
+            // makePie(tasks);
+            makePieDonut(tasks);
         };
 
         var failure = function () {
@@ -157,6 +158,127 @@ angular.module('done.tasks').controller('DoneCtrl', ['$scope', '$filter', '$stat
 
                         return data;
                     }())
+                }
+            ]
+        });
+    };
+
+    var makePieDonut = function (tasks) {
+
+        var pad = function (x, length) {
+            x = x + '';
+            while (x.length < length) {
+                x = '0' + x;
+            }
+            return x;
+        };
+
+        var getTimeFromMs = function (ms) {
+            var s, m, h,
+                time = '';
+
+            s = Math.round(ms / 1000);
+            m = Math.floor(s / 60);
+            if (m) {
+                s = s % 60;
+            }
+            h = Math.floor(m / 60);
+            if (h) {
+                m = m % 60;
+            }
+
+            if (s) {
+                s = pad(s, 2);
+                time = s;
+            }
+
+            if (m) {
+                m = pad(m, 2);
+                time = m + ':' + s;
+            }
+
+            if (h) {
+                time = h + ':' + time;
+            }
+
+            return time;
+        };
+
+        var formatter = function () {
+            var name = this.point.name,
+                timeString = getTimeFromMs(this.point.y);
+
+            return name + ' ' + timeString;
+        };
+
+        var colors = Highcharts.getOptions().colors; 
+    
+        var taskData = [],
+            categories = [],
+            category = '',
+            total = 0;
+
+        angular.forEach(tasks, function (task, index, collection) {
+            var lastCategory = categories.slice(-1)[0],
+                thisCategory = {};
+
+            taskData.push({
+                name: task.description,
+                y: task.duration
+            });
+
+            // Is this a new category?
+            if (!lastCategory || lastCategory.name !== task.category) {
+                categories.push({
+                    name: task.category,
+                    y: 0
+                });
+            }
+
+            thisCategory = categories.slice(-1)[0];
+
+            thisCategory.y += task.duration;
+        });
+    
+        // Create the chart
+        $('#dummy-chart').highcharts({
+            chart: {
+                type: 'pie'
+            },
+            title: {
+                text: 'How I Spent My Time'
+            },
+            yAxis: {
+                title: {
+                    text: 'time'
+                }
+            },
+            plotOptions: {
+                pie: {
+                    shadow: false,
+                    center: ['50%', '50%']
+                }
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            series: [
+                {
+                    name: 'Category',
+                    data: categories,
+                    size: '60%',
+                    dataLabels: {
+                        enabled: false
+                    }
+                },
+                {
+                    name: 'Task',
+                    data: taskData,
+                    size: '80%',
+                    innerSize: '60%',
+                    dataLabels: {
+                        formatter: formatter
+                    }
                 }
             ]
         });
